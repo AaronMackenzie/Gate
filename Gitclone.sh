c96f180b04b9501e188dd1a4fe1201950368968b
AaronMackenzie <138335712+AaronMackenzie@users.noreply.github.com>
2024-03-26 02:33:34 -0500

    Create Gitclone.sh

---
+++ Gitclone.sh
@@ -0,0 +1,11 @@
+#The software package "Kite_trade" is a Python program designed to facilitate interaction with the website https://kite.zerodha.com. 
+#It serves the specific purpose of enabling algorithmic trading without relying on the official Kite API and accessing a Zerodha account. 
+#This approach circumvents the need for a subscription to the Kite API, which typically starts at ₹4000 per month.
+
+
+#To obtain the necessary files for utilizing the "Kite_trade" program, execute the following command in your terminal:
+git clone https://github.com/TradeViaPython/Kite_Zerodha.git
+
+#This command clones the repository containing the "kite_trade.py" file onto your local machine. 
+#It is essential to have the "kite_trade.py" file in the same directory as your algorithmic trading program whenever building such a program. 
+#The "kite_trade.py" file can be found within the "Kite_Zerodha" repository.
